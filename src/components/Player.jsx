b379cfc9f26d7f6bc01a9f9fb11697594b19a1d2
claudiney63 <claudineyryan75@gmail.com>
2024-04-06 01:20:05 -0300

    78. User Input & Two-Way-Binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  const [editName, setEditName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required defaultValue={name} />
+          <input type="text" required defaultValue={editName} onChange={(e) => setEditName(e.target.value)}/>
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{editName}</span>
         )}
         <span className="player-symbol">{symbol}</span>
       </span>
