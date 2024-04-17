66f5742e0c3b7ee4969f5142c318c31aaafb683d
claudiney63 <claudineyryan75@gmail.com>
2024-04-17 14:09:38 -0300

    88. Outsourcing Data Into A Separate File

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import Player from "./components/Player";
 import { useState } from "react";
+import { WINNING_COMBINATIONS } from "./winning-combination";
 
 function derivatedPlayer(log) {
   let currentPlayer = "X";
